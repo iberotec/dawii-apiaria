79187f2aac26a168de836243b8cbae97d73b7186
johannfjs <johannfjs@gmail.com>
2012-10-21 21:10:53 +0000

    Version Inicial

---
+++ Project_Apiaria/src/apiario/edu/pe/JMensajes.java
@@ -0,0 +1,21 @@
+package apiario.edu.pe;
+
+import java.io.Serializable;
+
+public class JMensajes implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	public static final void pinta(String Titulo, Object mensaje){
+		System.out.println("[MONKAR] > "+Titulo+" : "+mensaje);
+	}
+	
+	public static final void pinta(Object mensaje){
+		System.out.println("[MONKAR] > MSG : "+mensaje);
+	}
+
+}
