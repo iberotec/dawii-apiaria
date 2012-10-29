e08593dbca69cfc181fb643b93074a1ed2a8a202
diegotl6691@gmail.com <diegotl6691@gmail.com@04d3c91d-cb8f-2a5d-dc6a-badc7ee1f3fd>
2012-10-29 07:01:12 +0000

    se creo el MBApiario

---
+++ trunk/Proyecto_Apiaria/src/apiario/edu/pe/manageBeans/MBApiario.java
@@ -0,0 +1,17 @@
+package apiario.edu.pe.manageBeans;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import apiario.edu.pe.bean.Apiario;
+
+public class MBApiario {
+	Apiario apiario;
+	List<Apiario> lista = new ArrayList<Apiario>();
+	
+	public void ubicacionApiario(){
+		System.out.println("entrega ubicacion");
+	}
+}
