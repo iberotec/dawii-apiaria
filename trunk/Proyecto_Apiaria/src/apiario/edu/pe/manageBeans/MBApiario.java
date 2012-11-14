a871641b9fad2d9bc73607c4ca546657dd9026f3
diegotl6691@gmail.com <diegotl6691@gmail.com@04d3c91d-cb8f-2a5d-dc6a-badc7ee1f3fd>
2012-11-14 07:12:52 +0000

    ordenando algunas cosas

--- trunk/Proyecto_Apiaria/src/apiario/edu/pe/manageBeans/MBApiario.java
+++ trunk/Proyecto_Apiaria/src/apiario/edu/pe/manageBeans/MBApiario.java
@@ -2,16 +2,22 @@ package apiario.edu.pe.manageBeans;
 
 
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import apiario.edu.pe.bean.Apiario;
 
-public class MBApiario {
+@SuppressWarnings("serial")
+public class MBApiario implements Serializable{
 	Apiario apiario;
 	List<Apiario> lista = new ArrayList<Apiario>();
 	
 	public void ubicacionApiario(){
 		System.out.println("entrega ubicacion");
+	}
+	public String abrirVerificarApiario(){
+		System.out.println("abrirVerificarApiario");
+		return "successMain";
 	}
 }
